fbfdbb4b425ed758ae1c4cfe37a7bba688605f39
Stefanos Poulis <stefanos.poulis@gmail.com>
2019-03-03 16:38:55 -0800

    adding inference

--- interactive_tm_code/R/infer.topics.R
+++ interactive_tm_code/R/infer.topics.R
@@ -1,5 +1,5 @@
 infer.topics	<-	function(bow,topic_counts,topic_sums,alpha,eta,n_iters,arora=TRUE){
-						
+						#Perform topic inference
 						model		<-	lda.collapsed.gibbs.sampler(documents= bow,K=nrow(topic_counts),vocab=colnames(topic_counts) ,n_iters,alpha,eta, initial=list(topics=topic_counts,topic_sums=topic_sums), freeze.topics=TRUE,compute.log.likelihood=TRUE)
 							
 						return(model)
